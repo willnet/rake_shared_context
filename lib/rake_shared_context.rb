5fdebb3b5cd9ccf07858fb617b248091ca44b667
willnet <netwillnet@gmail.com>
2013-04-05 14:12:56 +0900

    Delete unnesesarry method and variable

--- lib/rake_shared_context.rb
+++ lib/rake_shared_context.rb
@@ -6,17 +6,13 @@ begin
   shared_context "rake" do
     let(:rake)      { Rake::Application.new }
     let(:task_name) { self.class.top_level_description }
-    let(:task_path) { "lib/tasks/#{task_name.split(":").first}" }
     subject         { rake[task_name] }
 
-    def loaded_files_excluding_current_rake_file
-      $".reject {|file| file == Rails.root.join("#{task_path}.rake").to_s }
-    end
-
     before do
+      loaded_files = []
       Rake.application = rake
       Dir::glob("lib/tasks/*.rake").each do |task|
-        Rake.application.rake_require(task.sub(/.rake$/,''), [Rails.root.to_s], loaded_files_excluding_current_rake_file)
+        Rake.application.rake_require(task.sub(/.rake$/,''), [Rails.root.to_s], loaded_files)
       end
        
       Rake::Task.define_task(:environment)
